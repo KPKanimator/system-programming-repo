e8361affbfc10c926566bf9a632416566d7b9af4
Kirill <k.korobchinskiy@khai.edu>
2023-12-12 13:01:11 +0200

    Added Demo10 Lec

---
+++ LabWork10/LDemo01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    double *buffer;
+    int size = 10;
+
+    /* Динамічне виділення пам'яті */
+    if ((buffer = malloc(size * sizeof(double))) == NULL)
+    {
+        perror("malloc()");
+        return 1;
+    }
+    /* Робота з пам'яттю */
+    for (int i = 0; i < size; i++)
+    {
+        buffer[i] = i;
+        printf("%lf\n", buffer[i]);
+    }
+    /* Звільнення пам'яті */
+    free(buffer);
+    return 0;
+}
